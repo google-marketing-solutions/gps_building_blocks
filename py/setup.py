492005db9a7c34b2648de8b7335bdbdd18ffb13b
gPS team <no-reply@google.com>
2020-08-05 09:48:39 -0700

    Update setup.py with release version.
    PiperOrigin-RevId: 325038649

--- py/setup.py
+++ py/setup.py
@@ -26,7 +26,7 @@ with open("../README.md", "r") as fh:
 
 setuptools.setup(
     name="gps-building-blocks",
-    version="0.1.12",
+    version="1.0.0",
     author="gPS Team",
     author_email="no-reply@google.com",
     description="Modules and tools useful for use with advanced data solutions on Google Ads, Google Marketing Platform and Google Cloud.",
